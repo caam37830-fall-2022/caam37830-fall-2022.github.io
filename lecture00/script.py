1530dd6539bf1a83b609d7cd69ee976425bfff52
jghoskins <jeremyhoskins@gmail.com>
2022-09-27 21:02:52 -0500

    lecture0 material added

---
+++ lecture00/script.py
@@ -0,0 +1,23 @@
+"""
+A simple python script
+
+run from the terminal using
+$ python script.py
+"""
+
+# printing will print to the terminal
+print("hello world")
+
+# you can do anything in your script
+# define functions or classes
+# import packages
+# and so on...
+def f(a, b):
+    """
+    returns the sum of a and b
+    """
+    return a + b
+
+a = 1
+b = 2
+print(f"{a} + {b} = {f(a,b)}")
